7fce97a0d1c087d6793cad3bfc58eefe5b246e50
LucasWongC <wm111898@gmail.com>
2025-01-14 08:44:28 -0800

    init of wrapper

--- src/StkGhoERC4626Wrapper.sol
+++ src/StkGhoERC4626Wrapper.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import {StakeTokenERC4626Wrapper, IERC20, IStakeToken, IERC4626} from "./StakeTokenERC4626Wrapper.sol";
+import {IStakeToken} from "./interfaces/IStakeToken.sol";
+import {IERC4626, IERC20, IERC20Metadata} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IQuoter} from "./interfaces/IQuoter.sol";
 import {ISwapRouter} from "./interfaces/ISwapRouter.sol";
 import {IGsm} from "./interfaces/IGsm.sol";
 
-contract StkGhoERC4626Wrapper is StakeTokenERC4626Wrapper {
+import "forge-std/Test.sol";
+
+contract StkGhoERC4626Wrapper is IERC4626, ERC20 {
     address public constant STK_GHO =
         0x1a88Df1cFe15Af22B3c4c783D4e6F7F9e0C1885d;
+    address public constant GHO = 0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f;
 
     address public constant AAVE = 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
@@ -20,17 +25,238 @@ contract StkGhoERC4626Wrapper is StakeTokenERC4626Wrapper {
     ISwapRouter public constant SWAP_ROUTER =
         ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
 
-    constructor() StakeTokenERC4626Wrapper(STK_GHO) {}
+    modifier withMigrate(uint256 keepAssets) {
+        uint256 rewards = IStakeToken(STK_GHO).getTotalRewardsBalance(
+            address(this)
+        );
+        if (rewards > 0) {
+            _migrateRewards(keepAssets);
+        }
+        _;
+    }
+
+    constructor() ERC20("Wrapped StkGho", "WStkGho") {}
+
+    /* --- IERC4626 --- */
+    /// @inheritdoc IERC4626
+    function asset() external pure returns (address) {
+        return GHO;
+    }
+
+    /// @inheritdoc IERC4626
+    function totalAssets() public view returns (uint256) {
+        uint256 rewards = IStakeToken(STK_GHO).getTotalRewardsBalance(
+            address(this)
+        );
+        uint256 shares = IERC20(STK_GHO).balanceOf(address(this));
+
+        uint256 ghoFromReward = rewards == 0
+            ? 0
+            : _rewardToUnderlyingExactInput(rewards);
+        uint256 ghoFromShare = shares == 0
+            ? 0
+            : IStakeToken(STK_GHO).previewRedeem(shares);
+
+        return ghoFromReward + ghoFromShare;
+    }
+
+    /// @inheritdoc IERC4626
+    function convertToShares(uint256 assets) external view returns (uint256) {
+        return _assetToShare(assets);
+    }
+
+    /// @inheritdoc IERC4626
+    function convertToAssets(uint256 shares) external view returns (uint256) {
+        return _shareToAsset(shares);
+    }
+
+    /// @inheritdoc IERC4626
+    function maxDeposit(
+        address // receiver
+    ) external pure virtual returns (uint256 maxAssets) {
+        return 2 ** 256 - 1;
+    }
+
+    /// @inheritdoc IERC4626
+    function previewDeposit(uint256 assets) external view returns (uint256) {
+        return _assetToShare(assets);
+    }
+
+    /// @inheritdoc IERC4626
+    function deposit(
+        uint256 assets,
+        address receiver
+    ) external withMigrate(type(uint256).max) returns (uint256 shares) {
+        shares = _assetToShare(assets);
+
+        IERC20(GHO).transferFrom(msg.sender, address(this), assets);
+
+        uint256 stakeAmount = IERC20(GHO).balanceOf(address(this));
+        IERC20(GHO).approve(STK_GHO, stakeAmount);
+        IStakeToken(STK_GHO).stake(address(this), stakeAmount);
+
+        _mint(receiver, shares);
+    }
+
+    /// @inheritdoc IERC4626
+    function maxMint(
+        address // receiver
+    ) external pure virtual returns (uint256) {
+        return 2 ** 256 - 1;
+    }
+
+    /// @inheritdoc IERC4626
+    function previewMint(uint256 shares) external view returns (uint256) {
+        return _shareToAsset(shares);
+    }
+
+    /// @inheritdoc IERC4626
+    function mint(
+        uint256 shares,
+        address receiver
+    ) external withMigrate(type(uint256).max) returns (uint256 assets) {
+        assets = _shareToAsset(shares);
+
+        IERC20(GHO).transferFrom(msg.sender, address(this), assets);
+
+        uint256 stakeAmount = IERC20(GHO).balanceOf(address(this));
+        IERC20(GHO).approve(STK_GHO, stakeAmount);
+        IStakeToken(STK_GHO).stake(address(this), stakeAmount);
+
+        _mint(receiver, shares);
+    }
 
     /// @inheritdoc IERC4626
     function maxWithdraw(
         address owner
-    ) public view override returns (uint256 maxAssets) {
-        uint256 shares = IERC20(STAKE_TOKEN).balanceOf(owner);
-        uint256 rewards = IStakeToken(STAKE_TOKEN).getTotalRewardsBalance(
-            owner
+    ) external view virtual returns (uint256 maxAssets) {
+        uint256 shares = balanceOf(owner);
+        maxAssets = _shareToAsset(shares);
+    }
+
+    /// @inheritdoc IERC4626
+    function previewWithdraw(
+        uint256 assets
+    ) external view returns (uint256 shares) {
+        shares = _assetToShare(assets);
+    }
+
+    /// @inheritdoc IERC4626
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) external virtual returns (uint256 shares) {
+        uint256 swappedAsset = _migrateRewards(assets);
+        shares = _assetToShare(assets);
+
+        if (swappedAsset < assets) {
+            uint256 stkGhoRedeemAmount = IStakeToken(STK_GHO).previewStake(
+                assets - swappedAsset
+            );
+            IERC20(STK_GHO).approve(STK_GHO, stkGhoRedeemAmount);
+            IStakeToken(STK_GHO).redeem(address(this), stkGhoRedeemAmount);
+        }
+        IERC20(GHO).transfer(receiver, assets);
+
+        _burn(owner, shares);
+    }
+
+    /// @inheritdoc IERC4626
+    function maxRedeem(address owner) external view returns (uint256) {
+        uint256 shares = balanceOf(owner);
+        return shares;
+    }
+
+    /// @inheritdoc IERC4626
+    function previewRedeem(
+        uint256 shares
+    ) external view virtual returns (uint256 assets) {
+        assets = _shareToAsset(shares);
+    }
+
+    /// @inheritdoc IERC4626
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) external virtual returns (uint256 assets) {
+        assets = _shareToAsset(shares);
+        uint256 swappedAsset = _migrateRewards(assets);
+
+        if (swappedAsset < assets) {
+            uint256 stkGhoRedeemAmount = IStakeToken(STK_GHO).previewStake(
+                assets - swappedAsset
+            );
+            IERC20(STK_GHO).approve(STK_GHO, stkGhoRedeemAmount);
+            IStakeToken(STK_GHO).redeem(address(this), stkGhoRedeemAmount);
+        }
+        IERC20(GHO).transfer(receiver, assets);
+
+        _burn(owner, shares);
+    }
+
+    function _assetToShare(
+        uint256 assets
+    ) internal view returns (uint256 shares) {
+        uint256 totalAssetsTemp = totalAssets();
+        if (totalAssetsTemp == 0) {
+            shares = assets;
+        } else {
+            shares = (assets * totalSupply()) / totalAssetsTemp;
+        }
+    }
+
+    function _shareToAsset(
+        uint256 shares
+    ) internal view returns (uint256 assets) {
+        uint256 totalSupplyTemp = totalSupply();
+        if (totalSupplyTemp == 0) {
+            assets = shares;
+        } else {
+            assets = (shares * totalAssets()) / totalSupplyTemp;
+        }
+    }
+
+    function _migrateRewards(
+        uint256 keepAssets
+    ) internal returns (uint256 assets) {
+        uint256 rewards = IStakeToken(STK_GHO).getTotalRewardsBalance(
+            address(this)
+        );
+        IStakeToken(STK_GHO).claimRewards(address(this), rewards);
+
+        IERC20(AAVE).approve(address(SWAP_ROUTER), rewards);
+
+        uint256 usdcAmount = SWAP_ROUTER.exactInputSingle(
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: AAVE,
+                tokenOut: USDC,
+                fee: AAVE_USDC_UNIV3_FEE,
+                recipient: address(this),
+                deadline: block.timestamp + 100,
+                amountIn: rewards,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            })
         );
 
+        IERC20(USDC).approve(address(USDC_GSM), usdcAmount);
+        (, assets) = USDC_GSM.sellAsset(usdcAmount, address(this));
+
+        uint256 stakeAmounts = keepAssets < assets
+            ? assets - keepAssets
+            : assets;
+
+        if (stakeAmounts > 0) {
+            IERC20(GHO).approve(STK_GHO, assets);
+            IStakeToken(STK_GHO).stake(address(this), assets);
+        }
+    }
+
+    function _rewardToUnderlyingExactInput(
+        uint256 rewards
+    ) internal view returns (uint256 underlying) {
         bytes memory quoterCallData = abi.encodeWithSignature(
             "quoteExactInputSingle(address,address,uint24,uint256,uint160)",
             AAVE,
@@ -44,140 +270,6 @@ contract StkGhoERC4626Wrapper is StakeTokenERC4626Wrapper {
         require(success, "staticcall failed");
         uint256 usdcAmount = abi.decode(data, (uint256));
 
-        (, uint256 ghoAmounts, , ) = USDC_GSM.getGhoAmountForSellAsset(
-            usdcAmount
-        );
-        uint256 redeemAmount = IStakeToken(STAKE_TOKEN).previewRedeem(shares);
-
-        maxAssets = redeemAmount + ghoAmounts;
-    }
-
-    function previewClaim(
-        uint256 assets
-    ) public view returns (uint256 rewards) {
-        (uint256 usdcAmount, , , ) = USDC_GSM.getAssetAmountForSellAsset(
-            assets
-        );
-        bytes memory quoterCallData = abi.encodeWithSignature(
-            "quoteExactOutputSingle(address,address,uint24,uint256,uint160)",
-            AAVE,
-            USDC,
-            AAVE_USDC_UNIV3_FEE,
-            usdcAmount,
-            0
-        );
-        (bool success, bytes memory data) = QUOTER.staticcall(quoterCallData);
-        require(success, "staticcall failed");
-
-        rewards = abi.decode(data, (uint256));
-    }
-
-    /// @inheritdoc IERC4626
-    function withdraw(
-        uint256 assets,
-        address receiver,
-        address owner
-    ) external override returns (uint256 redeemShares) {
-        uint256 maxAssets = maxWithdraw(owner);
-        uint256 claimAmount = maxAssets > assets ? assets : maxAssets;
-        uint256 rewards = previewClaim(claimAmount);
-        bytes memory data = abi.encodeWithSignature(
-            "claimRewards(address,uint256)",
-            address(this),
-            rewards
-        );
-
-        (bool success, ) = STAKE_TOKEN.delegatecall(data);
-
-        require(success, "Delegatecall failed");
-
-        IERC20(AAVE).approve(address(SWAP_ROUTER), rewards);
-        uint256 usdcAmount = SWAP_ROUTER.exactInputSingle(
-            ISwapRouter.ExactInputSingleParams({
-                tokenIn: AAVE,
-                tokenOut: USDC,
-                fee: AAVE_USDC_UNIV3_FEE,
-                recipient: address(this),
-                deadline: block.timestamp + 100,
-                amountIn: rewards,
-                amountOutMinimum: 0,
-                sqrtPriceLimitX96: 0
-            })
-        );
-
-        IERC20(USDC).approve(address(USDC_GSM), usdcAmount);
-        (, uint256 ghoAmount) = USDC_GSM.sellAsset(usdcAmount, address(this));
-        if (ghoAmount < claimAmount) revert();
-
-        if (ghoAmount < assets) {
-            redeemShares = IStakeToken(STAKE_TOKEN).previewStake(
-                assets - ghoAmount
-            );
-            uint256 shares = IERC20(STAKE_TOKEN).balanceOf(owner);
-            if (shares < redeemShares) revert();
-
-            data = abi.encodeWithSignature(
-                "redeem(address,uint256)",
-                address(this),
-                redeemShares
-            );
-
-            (success, ) = STAKE_TOKEN.delegatecall(data);
-
-            require(success, "Delegatecall failed");
-        }
-
-        IERC20(ASSET).transfer(receiver, assets);
-    }
-
-    /// @inheritdoc IERC4626
-    function redeem(
-        uint256 shares,
-        address receiver,
-        address owner
-    ) external override returns (uint256 assets) {
-        uint256 rewards = IStakeToken(STAKE_TOKEN).getTotalRewardsBalance(
-            owner
-        );
-        bytes memory data = abi.encodeWithSignature(
-            "claimRewards(address,uint256)",
-            address(this),
-            rewards
-        );
-
-        (bool success, ) = STAKE_TOKEN.delegatecall(data);
-
-        require(success, "Delegatecall failed");
-
-        IERC20(AAVE).approve(address(SWAP_ROUTER), rewards);
-        uint256 usdcAmount = SWAP_ROUTER.exactInputSingle(
-            ISwapRouter.ExactInputSingleParams({
-                tokenIn: AAVE,
-                tokenOut: USDC,
-                fee: AAVE_USDC_UNIV3_FEE,
-                recipient: address(this),
-                deadline: block.timestamp + 100,
-                amountIn: rewards,
-                amountOutMinimum: 0,
-                sqrtPriceLimitX96: 0
-            })
-        );
-
-        IERC20(USDC).approve(address(USDC_GSM), usdcAmount);
-        USDC_GSM.sellAsset(usdcAmount, address(this));
-
-        data = abi.encodeWithSignature(
-            "redeem(address,uint256)",
-            address(this),
-            shares
-        );
-
-        (success, ) = STAKE_TOKEN.delegatecall(data);
-
-        require(success, "Delegatecall failed");
-
-        assets = IERC20(ASSET).balanceOf(address(this));
-
-        IERC20(ASSET).transfer(receiver, assets);
+        (, underlying, , ) = USDC_GSM.getGhoAmountForSellAsset(usdcAmount);
     }
 }
--- test/StkGhoERC4626WrapperTest.t.sol
+++ test/StkGhoERC4626WrapperTest.t.sol
@@ -1,62 +1,97 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import {Test} from "forge-std/Test.sol";
+import "forge-std/Test.sol";
 import {StkGhoERC4626Wrapper} from "src/StkGhoERC4626Wrapper.sol";
 import {IERC4626, IERC20, IERC20Metadata} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 contract StkGhoERC4626WrapperTest is Test {
     StkGhoERC4626Wrapper public stkGhoWrapper;
     address public stkGho;
+    address public gho;
 
     address public alice;
     address public bob;
 
     function setUp() public {
-        vm.createSelectFork("mainnet");
+        vm.createSelectFork("mainnet", 21623283);
         stkGhoWrapper = new StkGhoERC4626Wrapper();
         stkGho = stkGhoWrapper.STK_GHO();
+        gho = stkGhoWrapper.GHO();
 
         alice = makeAddr("alice");
         bob = makeAddr("bob");
 
-        deal(stkGho, alice, 1_000e18);
+        deal(gho, alice, 1_000e18);
+    }
+
+    function test_default() public {
+        assertEq(stkGhoWrapper.asset(), gho);
+        assertEq(stkGhoWrapper.maxDeposit(address(0)), 2 ** 256 - 1);
+        assertEq(stkGhoWrapper.maxMint(address(0)), 2 ** 256 - 1);
     }
 }
 
-contract ERC20Test is StkGhoERC4626WrapperTest {
-    function test_IERC20Metadata_functions() public {
-        assertEq(stkGhoWrapper.name(), IERC20Metadata(stkGho).name());
-        assertEq(stkGhoWrapper.symbol(), IERC20Metadata(stkGho).symbol());
-        assertEq(stkGhoWrapper.decimals(), IERC20Metadata(stkGho).decimals());
-    }
+contract DepositAndMintTest is StkGhoERC4626WrapperTest {
+    uint256 public constant amount = 100e18;
 
-    function test_IERC20_view_functions() public {
-        vm.prank(alice);
-        IERC20(stkGho).approve(bob, 1e18);
-        assertEq(stkGhoWrapper.totalSupply(), IERC20(stkGho).totalSupply());
-        assertEq(
-            stkGhoWrapper.balanceOf(alice),
-            IERC20(stkGho).balanceOf(alice)
-        );
-        assertEq(
-            stkGhoWrapper.allowance(alice, bob),
-            IERC20(stkGho).allowance(alice, bob)
-        );
-    }
-
-    function test_transfer() public {
-        uint256 aliceBalanceBefore = stkGhoWrapper.balanceOf(alice);
-        uint256 bobBalanceBefore = stkGhoWrapper.balanceOf(bob);
+    function test_deposit() public {
+        uint256 totalAssetsBefore = stkGhoWrapper.totalAssets();
+        uint256 previewedDeposit = stkGhoWrapper.previewDeposit(amount);
 
         vm.startPrank(alice);
-        assertTrue(stkGhoWrapper.transfer(bob, 1e18));
+        IERC20(gho).approve(address(stkGhoWrapper), amount);
+        stkGhoWrapper.deposit(amount, alice);
 
-        uint256 aliceBalanceAfter = stkGhoWrapper.balanceOf(alice);
-        uint256 bobBalanceAfter = stkGhoWrapper.balanceOf(bob);
+        uint256 totalAssetsAfter = stkGhoWrapper.totalAssets();
+        uint256 stkGhoBalance = IERC20(stkGho).balanceOf(
+            address(stkGhoWrapper)
+        );
+        uint256 wStkGhoBalance = stkGhoWrapper.balanceOf(alice);
 
-        assertEq(aliceBalanceBefore, aliceBalanceAfter + 1e18);
-        assertEq(bobBalanceBefore, bobBalanceAfter - 1e18);
+        assertEq(totalAssetsAfter, totalAssetsBefore + amount);
+        assertEq(previewedDeposit, amount);
+        assertEq(stkGhoBalance, amount);
+        assertEq(wStkGhoBalance, amount);
+
+        vm.stopPrank();
+    }
+
+    function test_mint() public {
+        uint256 totalAssetsBefore = stkGhoWrapper.totalAssets();
+        uint256 previewedMint = stkGhoWrapper.previewMint(amount);
+
+        vm.startPrank(alice);
+        IERC20(gho).approve(address(stkGhoWrapper), amount);
+        stkGhoWrapper.mint(amount, alice);
+
+        uint256 totalAssetsAfter = stkGhoWrapper.totalAssets();
+        uint256 stkGhoBalance = IERC20(stkGho).balanceOf(
+            address(stkGhoWrapper)
+        );
+        uint256 wStkGhoBalance = stkGhoWrapper.balanceOf(alice);
+
+        assertEq(totalAssetsAfter, totalAssetsBefore + amount);
+        assertEq(previewedMint, amount);
+        assertEq(stkGhoBalance, amount);
+        assertEq(wStkGhoBalance, amount);
+
+        vm.stopPrank();
+    }
+
+    function test_depositWithMigrate() public {
+        vm.startPrank(alice);
+
+        IERC20(gho).approve(address(stkGhoWrapper), amount);
+        stkGhoWrapper.deposit(amount, alice);
+
+        uint256 totalAssetsBefore = stkGhoWrapper.totalAssets();
+        vm.warp(block.timestamp + 30 * 24 * 60 * 60);
+
+        uint256 totalAssetsAfter = stkGhoWrapper.totalAssets();
+
+        assertGe(totalAssetsAfter, totalAssetsBefore);
+        console.log(totalAssetsAfter);
 
         vm.stopPrank();
     }
